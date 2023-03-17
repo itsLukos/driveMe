afc92a0e0de27a9e8e7699e555017d8403ebcd8e
Rubenprada <rubenprada89@outlook.com>
2023-03-17 19:04:20 +0100

    FEAT: users schema done

---
+++ models/Users.js
@@ -0,0 +1,34 @@
+
+
+//requerimos mongoose
+const mongoose = require('mongoose');
+
+//creamos el esquema
+const userSchema = new mongoose.Schema({
+    nombre: {type: String, required: true},
+    apellidos: {type: String, required: true},
+    telefono: {type: Number, required: true},
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // marcamos formato del email
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'El email introducido no tiene un formato válido']
+    },
+    password: {type: String, required: true},
+    //añadimos roles, si no se pone nada será usuario basico
+    role: { type: String, default: 'basic', enum: ['buyer', 'seller'] },
+    //relacionamos con la colección de Movies
+    favoriteCars: [{ type: mongoose.Types.ObjectId, ref: 'Cars' }],
+
+}, {
+    timestamps: true
+});
+
+//Creamos la coleccion, recibe 2 args
+//    1.Nombre de la coleccion
+//    2.Esquema que sigue que creamos arriba
+const User = mongoose.model('User', userSchema);
+
+//exportamos
+module.exports = User;
