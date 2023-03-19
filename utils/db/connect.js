35b1c25576da0f26a58ee918c3f1a38499ae0955
Rubenprada <rubenprada89@outlook.com>
2023-03-19 17:12:48 +0100

    FEAT: js file connect done

--- utils/db/connect.js
+++ utils/db/connect.js
@@ -1,8 +1,9 @@
+
 //requerimos mongoose
 const mongoose = require('mongoose');
 
 //URL de la base de datos
-const DB_URL = "mongodb+srv://root:yKmprSQQn6lef7Hd@driveme.ohohktp.mongodb.net/?retryWrites=true&w=majority";
+const DB_URL = process.env.DB_URL;
 
 //funcion para conectar a la db
 const connect = () => {
