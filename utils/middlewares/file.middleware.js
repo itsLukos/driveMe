e4b0a89fd190bef713a7b3a21a0732bdedfaf6a9
Rubenprada <rubenprada89@outlook.com>
2023-03-29 11:20:28 +0200

    FEAT: Solved

--- utils/middlewares/file.middleware.js
+++ utils/middlewares/file.middleware.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
     },
     //donde se guarda
     destination: (req, file, cb) => {
-        cb(null, '/tmp/');
+        cb(null, path.join(__dirname, '../../public/uploads'));
     }
 });
 
